974e1054413e592a69b461d650f81a4cb1c4c1e7
Aryan <91008595+aryB003@users.noreply.github.com>
2021-11-23 10:18:47 +0400

    ....

--- routes/apis.js
+++ routes/apis.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const productController = require('../controllers/productController');
 
+const clientController = require('../controllers/clientController');
+const catalogServices = require('../services/productServices');
+
 //define a router and create routes
 const router = express.Router();
 
@@ -8,7 +11,13 @@ const router = express.Router();
 //-----------------------------------------------
 //route for listing all products
 router.get('/api/catalog', productController.getCatalogue);
-router.get('/api/article/:id', productController.getProductByID);
+router.get('/api/article/:id', (req, res) => {
+    console.log(req.params.id)
+    catalogServices.searchIDService(req.params.id, function(err, rows) {
+        res.render('article', { product: rows });
+
+    });
+});
 
 //export router
 module.exports = router;
